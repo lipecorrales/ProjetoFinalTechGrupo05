49acc63230fb15bec2ea1f24c16d9b492066a1a0
Philippe Corrales <philippecorrales@gmail.com>
2021-12-28 18:01:57 -0300

    Delete projetogrupo05 directory

--- projetogrupo05/src/main/java/com/projetogrupo05/projetogrupo05/repository/AccountRepo.java
+++
@@ -1,8 +0,0 @@
-package com.projetogrupo05.projetogrupo05.repository;
-
-import com.projetogrupo05.projetogrupo05.model.AccountBancaria;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccountRepo extends CrudRepository<AccountBancaria, Long> {
-    
-}
--- projetogrupo05/src/main/java/com/projetogrupo05/projetogrupo05/repository/ClientRepo.java
+++
@@ -1,8 +0,0 @@
-package com.projetogrupo05.projetogrupo05.repository;
-
-import com.projetogrupo05.projetogrupo05.model.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepo extends CrudRespository<Client, Long> {
-    
-}
